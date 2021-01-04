22f6a91e0efb1b68acf87a0f140f57a7ae44fbce
Arun Sudharsan <sudarun@amazon.com>
2021-01-04 15:37:04 +0530

    Navigation Component - MVVM To-Do List App with Flow and Architecture Components #3

--- app/src/main/java/io/arunbuilds/doit/ui/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/doit/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.arunbuilds.doit
+package io.arunbuilds.doit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.arunbuilds.doit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/io/arunbuilds/doit/ui/tasks/TasksFragment.kt
@@ -0,0 +1,6 @@
+package io.arunbuilds.doit.ui.tasks
+
+import androidx.fragment.app.Fragment
+import io.arunbuilds.doit.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks)
