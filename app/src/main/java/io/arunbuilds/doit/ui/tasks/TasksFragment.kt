56da5027a4ab34ee6ed886fc9bf9056d57640b82
Arun Sudharsan <sudarun@amazon.com>
2021-01-04 22:07:27 +0530

    Restoring the Fragment SearchView - MVVM To-Do List App with Flow and Architecture Components #14

--- app/src/main/java/io/arunbuilds/doit/ui/tasks/TasksFragment.kt
+++ app/src/main/java/io/arunbuilds/doit/ui/tasks/TasksFragment.kt
@@ -30,6 +30,7 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClickListener {
     private val viewModel: TasksViewModel by viewModels()
+    private lateinit var searchView: SearchView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -112,7 +113,12 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
         inflater.inflate(R.menu.menu_fragment_tasks, menu)
 
         val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
+        searchView = searchItem.actionView as SearchView
+        val pendingQuery = viewModel.searchQuery.value
+        if(pendingQuery!= null && pendingQuery.isNotEmpty()) {
+            searchItem.expandActionView()
+            searchView.setQuery(pendingQuery, false)
+        }
         searchView.onQueryTextChanged {
             // update search query
             viewModel.searchQuery.value = it
@@ -148,5 +154,10 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        searchView.setOnQueryTextListener(null)
     }
 }
